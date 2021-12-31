2698dbd26117d556a5ca1888620face7c491c965
NickSagan <9952728@gmail.com>
2021-12-31 15:56:29 +0300

    add outlets

--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextfield: UITextField!
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
